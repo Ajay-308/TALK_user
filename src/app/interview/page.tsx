bdbda0bc85cde485acdb179b4d9ed201a7c88551
Ajay <112794893+Ajay-308@users.noreply.github.com>
2024-01-25 00:45:03 +0530

    beauti of my code performing interview

--- src/app/interview/page.tsx
+++ src/app/interview/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useState } from "react";
 import axios from "axios";
+import Navbar from "../home/Navbar";
+import { Button } from "@/components/ui/button";
 
 const ChatComponent: React.FC = () => {
   const [inputMessage, setInputMessage] = useState<string>("");
@@ -26,25 +28,52 @@ const ChatComponent: React.FC = () => {
   };
 
   return (
-    <div>
-      <div>
-        <textarea
-          rows={10}
-          cols={50}
-          value={chatHistory}
-          readOnly
-          style={{ marginBottom: "10px" }}
-        />
+    // <div>
+    //   <div>
+    //     <textarea
+    //       rows={10}
+    //       cols={50}
+    //       value={chatHistory}
+    //       readOnly
+    //       style={{ marginBottom: "10px" }}
+    //     />
+    //   </div>
+    //   <div>
+    //     <input
+    //       type="text"
+    //       value={inputMessage}
+    //       onChange={(e) => setInputMessage(e.target.value)}
+    //     />
+    //     <button onClick={handleSendMessage}>Send</button>
+    //   </div>
+    // </div>
+    <>
+      <Navbar />
+      <div className="container-fluid  bg-gradient text-dark mt-5 flex flex-col items-center justify-center">
+        <div className="border-warning border-3 h-[3rem] w-[15rem] rounded shadow">
+          <h1 className="  mb-8 ml-4 mt-4"> Interview Prep with AI</h1>
+        </div>
+        <div className="mt-4">
+          <div>
+            <textarea
+              rows={10}
+              cols={50}
+              value={chatHistory}
+              readOnly
+              style={{ marginBottom: "10px" }}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              value={inputMessage}
+              onChange={(e) => setInputMessage(e.target.value)}
+            />
+            <Button onClick={handleSendMessage}>Send</Button>
+          </div>
+        </div>
       </div>
-      <div>
-        <input
-          type="text"
-          value={inputMessage}
-          onChange={(e) => setInputMessage(e.target.value)}
-        />
-        <button onClick={handleSendMessage}>Send</button>
-      </div>
-    </div>
+    </>
   );
 };
 
